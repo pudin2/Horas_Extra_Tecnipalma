599366989c1ccfb889d44d1dca9878a030ac7619
pudin11 <je.beltranc1@uniandes.edu.co>
2025-01-04 12:03:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/horas_extra_tecnipalma/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.horas_extra_tecnipalma
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.horas_extra_tecnipalma.ui.theme.Horas_Extra_TecnipalmaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Horas_Extra_TecnipalmaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Horas_Extra_TecnipalmaTheme {
+        Greeting("Android")
+    }
+}
